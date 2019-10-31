4a4ed43c6b49ded032067fc3e9a5167434b4e59f
liuyuhuan <liuyuhuan@babytree-inc.com>
2019-10-31 18:38:29 +0800

    fix

--- pages/writeAdd/index.js
+++ pages/writeAdd/index.js
@@ -3,18 +3,31 @@ var app = getApp()
 Page({
   data: {
     items: [
-      { name: 'USA', value: '张小明,182****6219,地址：江苏省 南京市 建邺区 云锦路 汉中门大街151号XX层XX室' },
-      { name: 'CHN', value: '中国', checked: 'true' },
-      { name: 'BRA', value: '巴西' },
-      { name: 'JPN', value: '日本' },
-      { name: 'ENG', value: '英国' },
-      { name: 'FRA', value: '法国' },
+      { value: '张小明,182****6219,地址：江苏省 南京市 建邺区 云锦路 汉中门大街151号XX层XX室' },
+      { value: '张小明,182****6219,地址：江苏省 南京市 建邺区 云锦路 汉中门大街151号XX层XX室' },
+      { value: '张小明,182****6219,地址：' },
+      { value: '张小明,182****6219,地址：江苏省 南京市 建邺区 云锦路 汉中门大街151号XX层XX室' },
+      { value: '张小明,182****6219,地址：江苏省 南京市 建邺区 云锦路 汉中门大街151号XX层XX室' },
+      { value: '张小明,182****6219,地址：江苏省 南京市 建邺区 云锦路 汉中门大街151号XX层XX室' },
+      { value: '张小明,182****6219,地址：江苏省 南京市 建邺区 云锦路 汉中门大街151号XX层XX室' }
     ]
 
   },
   
   radioChange: function (e) {
     console.log('radio发生change事件，携带value值为：', e.detail.value)
+  },
+  gotoUrlAddress:function(e){
+    var obj = e.currentTarget;
+    var type = obj.dataset['type'];
+    if(type !== undefined){
+      app.data.addressEditor = type;
+    }else{
+      app.data.addressEditor = '';
+    }
+    wx.navigateTo({
+      url: '../addAddress/index'
+    })
   },
   onLoad: function () {
 
@@ -34,16 +47,7 @@ Page({
       }
 
     })
-    let data = app.data.answer.items[0];
-    this.setData({
-      img: data.picURL,
-      name: data.spec.split(',')[0] + data.itemName,
-      price1: data.spec.split(',')[0],
-      price2: '',
-      userName: '',
-      userTellphone: '',
-      userAdd: ''
-    })
+    
   },
 
 })
